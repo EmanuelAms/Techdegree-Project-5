ffb56a75a26ac613bdb63f09acbe044bfe86e4c7
Emanuel Ams <emanuel.amselem@gmail.com>
2020-11-24 20:15:02 +0100

    update
    wip

---
+++ js/scripts.js
@@ -0,0 +1,72 @@
+/*
+/
+/
+*/
+
+//
+
+add DOM elements to html
+add Search markup: 
+add Gallery markup:
+add Modal markup:
+
+/*
+/
+/
+*/
+
+//
+
+element.innerHTML += 'HTML string' >>> element.insertAdjacentHTML('beforeend', 'HTML string')
+
+/*
+/
+/
+*/
+
+//
+
+send a single request to the API
+use the response data to display 12 users (imitate mockups.png, look up index.html comments)
+                         display Image , First and Last Name , Email , City or location
+> 12 random users are pulled from the API in a single request
+> New random employee information displays each time the page refreshes 
+> The directory displays 12 users from the Random User API
+
+// EXTRA CREDITS 
+
+SEARCH
+Filter directory by name, filters results that are already on the page
+adjust your API request to retrieve a user nationality that will only return data in the English alphabet.
+don't request new info from the API for your search.
+(look up index.html comments)
+
+/*
+/
+/
+*/
+
+//
+
+click > modal window pop up with details displayed : Image, Name, Email, City or location, Cell Number (XXX) XXX-XXXX, Birthday MM/DD/YYYY,
+Detailed Address, including street name and number, state or country, and two letter post code.
+(imitate mockups.png, look up index.html comments)
+Find a way to close the modal window
+
+// EXTRA CREDITS 
+
+TOGGLE
+FInd a way toggle back and forth between employees when the modal window is open.
+There should be no errors once the end or beginning of the list is reached.
+(look up index.html comments)
+
+/*
+/
+/
+*/
+
+// EXTRA CREDITS 
+
+CSS
+change color / background color / font / box or text shadows & document changes in readme and submission notes
+Don't alter the layout or position of the important elements on the page.
