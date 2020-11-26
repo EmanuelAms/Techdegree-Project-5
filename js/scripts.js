2212cf39159a2e55efb2349a564b75745e47fa8e
Emanuel Ams <emanuel.amselem@gmail.com>
2020-11-26 20:30:09 +0100

    Meets expectations
    submit for review

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,34 @@
+/*
+// API usage
+*/
+
+/*
+Fetch 12 random users from an API on the "randomuser.me" website.
+Store these 12 users in an object with the json method.
+Fill the "Employees" array with the 12 user objects.
+Display those 12 users on the page with the "showEmployees" function.
+*/
+
 let Employees = [] ;
-let clickedEmployee ;
 
 const APIurl = 'https://randomuser.me/api/?results=12' ;
 
 fetch(APIurl)
-.then(response => response.json())
-.then(data => Employees = data.results)
-.then(data => showEmployees(Employees))
-.then(data => showModal(clickedEmployee));
+    .then(response => response.json())
+    .then(data => Employees = data.results)
+    .then(data => showEmployees(Employees));
 
 
+/*
+// User directory
+*/
 
+/*
+Create a function "showEmployees" to dynamically add HTML code to the index.html file.
+Insert HTML code in the "gallery" div, so as to display 12 user "cards".
+For every user object of the "Employees" array, a new "card" div is written.
+Each "card" div contains the Employee's image, First and Last Name, Email, City and State.
+*/
 
 const galleryDiv = document.querySelector('#gallery') ;
 
@@ -35,7 +53,17 @@ function showEmployees (Employees) {
 };
 
 
+/*
+// Modal Window
+*/
 
+/*
+Add an event listener to respond to clicks on the 12 user "cards".
+Clicking on a user card calls the "showClickedEmployee" function, which determines which users has been called,
+and then calls this user's "modal window", a pop up window with further details.
+Clicking on a user updates the "clickedEmployeeMail" variable, with the user's email.
+This happens regardless of where the mouse clicks, in the user "card".
+*/
 
 let clickedEmployeeMail ;
 
@@ -54,21 +82,36 @@ galleryDiv.addEventListener('click', (e) => {
     }
 });
 
+/*
+Create a function "showClickedEmployee" to determine which is the "clickedEmployee", using the email of the employee as matching reference.
+It then calls for the "showModal" function which displays the pop up window.
+*/
 
+let clickedEmployee ;
 
 function showClickedEmployee () {
-for (let i = 0 ; i < Employees.length ; i++) {
-    if (Employees[i].email === clickedEmployeeMail) {
-        clickedEmployee = Employees[i] ;
-        showModal(clickedEmployee);
-  }
- }
+    for (let i = 0 ; i < Employees.length ; i++) {
+        if (Employees[i].email === clickedEmployeeMail) {
+            clickedEmployee = Employees[i] ;
+            showModal(clickedEmployee);
+        }
+     }
+};
 
-} ;
+/*
+Create a function "convertDob" to convert the date format of the users objects with a MM/DD/YYYY format.
+*/
 
 function convertDob (date) {
-    return date = date.substring(0,10).replace(/\D/g, '').replace(/(\d{4})(\d{2})(\d{2})/g, '$2/$3/$1');
-}
+    return date = date.substring(0,10).replace(/\D/g, '').replace(/(\d{4})(\d{2})(\d{2})/g, '$2/$3/$1')
+};
+
+/*
+Create a function "showModal" to display the pop up window of the clicked user card on the gallery.
+Dynamically add HTML code to the index.html file, right after the "gallery" div.
+Insert a new "modal-container" div, containing the Emloyee's image, First Name, Email, City, Cell number, Detailed Adress, and Birthday.
+The closing "modal-close-btn" button with the "X" icon can be clicked to close the pop up, by removing the HTML code.
+*/
 
 function showModal (clickedEmployee) {  
 
